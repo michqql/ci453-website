033ebe9e921c458eb158945a5e9f57361a797112
Michael <M.Pearson4@uni.brighton.ac.uk>
2023-03-10 13:30:47 +0000

    Accordion list 'single-active' class

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,29 @@
 const accordions = document.getElementsByClassName('accordion');
 
 for(let i = 0; i < accordions.length; i++) {
-    accordions[i].addEventListener('click', function() {
-        this.classList.toggle('active');
+    let currentAccordion = accordions[i]; // the current accordion in the array
+    let parent = currentAccordion.parentNode; // the parent of this element
+    // the children of the parent AKA the siblings of the current element
+    // siblings also includes the current element too
+    let siblings = parent.getElementsByClassName('accordion'); 
+    
+    // boolean whether the accordion-list (parent) should only have a
+    // single accordion active at a time, and should close the others
+    // when a different accordion is made active
+    let singleActiveBool = parent.classList.contains('single-active');
+
+    currentAccordion.addEventListener('click', function() {
+        // if single active class is applied to the parent,
+        // all other accordions should become inactive before this one is made active
+        if(this.classList.contains('active')) {
+            this.classList.remove('active');
+        } else {
+            if(singleActiveBool) {
+                for(item of siblings) {
+                    item.classList.remove('active');
+                }
+            }
+            this.classList.add('active');
+        }
     });
 }
